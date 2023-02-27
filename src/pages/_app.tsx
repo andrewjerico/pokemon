11ada7e331a80878957b53def0f26531f9af8dae
Andrew Jerico <andrew.jerico@binus.ac.id>
2023-02-27 23:15:28 +0700

    first

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import Header from '@/header'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/services/index.ts
@@ -0,0 +1,10 @@
+import Get from "./Get";
+
+//Get
+const getAllPokemon = (limit : number) => Get(limit);
+
+const API = {
+    getAllPokemon
+}
+
+export default API;
